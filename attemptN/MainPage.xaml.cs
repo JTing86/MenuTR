c89ace87f00f8f18077d53d7dcfd9106d60bd704
JTing86 <jingting9786@gmail.com>
2018-02-25 10:55:24 -0500

    Initial check-in of Presto-HTV

---
+++ attemptN/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Xamarin.Forms;
+
+//using System;
+
+namespace attemptN
+{
+    public partial class MainPage : ContentPage
+    {
+        /*
+        public MainPage()
+
+        {
+            InitializeComponent();
+            //add events here!
+            Button button = new Button
+            {
+                Text = "Navigate!",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+            button.Clicked += async (sender, args) =>
+            {
+                await Navigation.PushAsync(new homePage());
+            };
+            Content = button;
+        }
+            */
+        public MainPage()
+        {
+            InitializeComponent();
+
+            CameraButton.Clicked += CameraButton_Clicked;
+        }
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+        }
+    }
+}
---
+++ attemptN/homePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace attemptN
+{
+    public partial class homePage : ContentPage
+    {
+        public homePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
